f24f46c3c67f3984aa115c66e9227e6e3fb85c04
Hugh Collier <collier.289@gmail.com>
2024-02-28 17:16:34 +0000

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/css/style.css')
+  eleventyConfig.addPassthroughCopy('./src/js')
+  eleventyConfig.addPassthroughCopy('./src/imgages')
+
+  // Using Browser Sync because Eleventy Dev Server does not seem to hot-reload browser until browser window is active in Firefox on macOS when CSS file is changed (Seems fine for other file types 🤷‍♂️)
+  eleventyConfig.setServerOptions({
+    module: '@11ty/eleventy-server-browsersync',
+
+    // Default Browsersync options shown:
+    port: 8080,
+    open: true,
+    notify: false,
+    ui: false,
+    ghostMode: false,
+
+    // Opt-out of the Browsersync snippet
+    // snippet: false,
+  })
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+    },
+
+    // Set templating language for .md files to nunjucks (default is liquid)
+    markdownTemplateEngine: 'njk',
+  }
+}
---
+++ src/index.md
@@ -0,0 +1,7 @@
+---
+layout: 'base.njk'
+tag:
+title: This is the homepage
+---
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatum ab inventore doloremque minima corporis reprehenderit adipisci voluptatem velit suscipit, fugiat dolor libero dicta odit animi quisquam placeat, labore cumque?
