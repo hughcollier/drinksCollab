d34e0fbc5ba6c7b20d8495dd58128edc0d1cc706
Hugh Collier <collier.289@gmail.com>
2024-04-23 17:27:56 +0100

    wip commit - form in progress

--- .eleventy.js
+++ .eleventy.js
@@ -4,19 +4,19 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/images')
 
   // Using Browser Sync because Eleventy Dev Server does not seem to hot-reload browser until browser window is active in Firefox on macOS when CSS file is changed (Seems fine for other file types 🤷‍♂️)
-  eleventyConfig.setServerOptions({
-    module: '@11ty/eleventy-server-browsersync',
-
-    // Default Browsersync options shown:
-    port: 8080,
-    open: true,
-    notify: false,
-    ui: false,
-    ghostMode: false,
-
-    // Opt-out of the Browsersync snippet
-    // snippet: false,
-  })
+  //   eleventyConfig.setServerOptions({
+  //     module: '@11ty/eleventy-server-browsersync',
+  //
+  //     // Default Browsersync options shown:
+  //     port: 8080,
+  //     open: true,
+  //     notify: false,
+  //     ui: false,
+  //     ghostMode: false,
+  //
+  //     // Opt-out of the Browsersync snippet
+  //     // snippet: false,
+  //   })
 
   return {
     dir: {
